db763f726576ad1646aef178eda8ce5a15f8a703
NiclasNielsen <42369991+NiclasNielsen@users.noreply.github.com>
2021-05-13 16:31:23 +0200

    ocd

--- src/main.js
+++ src/main.js
@@ -14,74 +14,74 @@ var config = {
     storageBucket: "vesterhavsgruppen-16c58.appspot.com",
     messagingSenderId: "83457068600",
     appId: "1:83457068600:web:1bd3bdb683c644bfdea0cd"
-  };
-  // Initialize Firebase
-  const firebaseApp = firebase.initializeApp(config);
+};
+// Initialize Firebase
+const firebaseApp = firebase.initializeApp(config);
 
-  const db = firebaseApp.firestore()
-  // Lav en af dem her efter vær collection!
-  const usersCollection = db.collection('users')
-  const calendarCollection = db.collection('calendar')
+const db = firebaseApp.firestore()
+// Lav en af dem her efter vær collection!
+const usersCollection = db.collection('users')
+const calendarCollection = db.collection('calendar')
 
 
 
-  // Create
-  export const createUser = user => {
-      return usersCollection.add(user)
-  }
-  export const createCalendar = calendar => {
-      return calendarCollection.add(calendar)
-  }
-  // select single Read
-  export const getUser = async id => {
+// Create
+export const createUser = user => {
+    return usersCollection.add(user)
+}
+export const createCalendar = calendar => {
+    return calendarCollection.add(calendar)
+}
+// select single Read
+export const getUser = async id => {
     const user = await usersCollection.doc(id).get()
     return user.exists ? user.data() : null
-  }
-  export const getCalendar = async id => {
+}
+export const getCalendar = async id => {
     const calendar = await calendarCollection.doc(id).get()
     return calendar.exists ? calendar.data() : null
-  }
-  // Update
-  export const updateUser = (id, user) => {
-      return usersCollection.doc(id).update(user)
-  }
-  export const updateCalendar = (id, calendar) => {
-      return calendarCollection.doc(id).update(calendar)
-  }
+}
+// Update
+export const updateUser = (id, user) => {
+    return usersCollection.doc(id).update(user)
+}
+export const updateCalendar = (id, calendar) => {
+    return calendarCollection.doc(id).update(calendar)
+}
 
-  // Delete
-  export const deleteUser = id => {
-      return usersCollection.doc(id).delete()
-  }
-  export const deleteCalendar = id => {
-      return calendarCollection.doc(id).delete()
-  }
+// Delete
+export const deleteUser = id => {
+    return usersCollection.doc(id).delete()
+}
+export const deleteCalendar = id => {
+    return calendarCollection.doc(id).delete()
+}
 
-  // mass Read
-  export const useLoadUsers = () => {
-      const Users = ref([])
+// mass Read
+export const useLoadUsers = () => {
+    const Users = ref([])
 
-      const close = usersCollection.onSnapshot(snapshot => {
-        Users.value = snapshot.docs.map(doc => ({
-              id: doc.id,
-              ...doc.data()
-          }))
-      })
-      onUnmounted(close)
-      return Users
-  }
-  export const useLoadCalendars = () => {
-      const Calendars = ref([])
+    const close = usersCollection.onSnapshot(snapshot => {
+    Users.value = snapshot.docs.map(doc => ({
+            id: doc.id,
+            ...doc.data()
+        }))
+    })
+    onUnmounted(close)
+    return Users
+}
+export const useLoadCalendars = () => {
+    const Calendars = ref([])
 
-      const close = calendarCollection.onSnapshot(snapshot => {
-        Calendars.value = snapshot.docs.map(doc => ({
-              id: doc.id,
-              ...doc.data()
-          }))
-      })
-      onUnmounted(close)
-      return Calendars
-  }
+    const close = calendarCollection.onSnapshot(snapshot => {
+    Calendars.value = snapshot.docs.map(doc => ({
+            id: doc.id,
+            ...doc.data()
+        }))
+    })
+    onUnmounted(close)
+    return Calendars
+}
 
 
 
