be49094f3af24d2d9d2188d3b8643a0142f9e647
NiclasNielsen <42369991+NiclasNielsen@users.noreply.github.com>
2021-05-13 20:36:00 +0200

    Comments og Blur listener

--- src/components/CalendarCreate.vue
+++ src/components/CalendarCreate.vue
@@ -25,6 +25,7 @@
       <div>
         <label for="start">Dato</label>
         <input 
+          @blur="spawnDayAttachments"
           type="date"
           v-model="form.start"
           required
@@ -35,6 +36,7 @@
       <div>
         <label for="end">Til</label>
         <input 
+          @blur="spawnDayAttachments"
           type="date"
           v-model="form.end"
           id="end"
@@ -50,27 +52,52 @@
 
 <script>
 import { reactive } from 'vue'
-import { createCalendar } from '@/main.js' // maybe this make error
+import { createCalendar } from '@/main.js'
 
   export default {
     setup() {
       const monthLengths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30];
       const monthLengthsLeap = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30];
 
+      const spawnDayAttachments = () => {
+        if(form.start != '' && form.end != ''){
+          console.log(dayCalc().amountOfDays)
+        }
+      }
+
       const yearCalc = (time) => {
         /* Leap years makes things complicated, okay?! */
         /* 
-          Gotta be honest i just hit my head against
-          the wall and tried every theory that poped
-          up until i got something that worked.
-
           It is tested it is working perfectly.
           It is centered around 1. jan 2020
           so anything before that is not tested
           and will most likely not work
         */
 
+        /* Time er delt op YYYY MM DD */
+        /* Så time[0] = med år */
         let year = time[0];
+        
+        /* 
+          Jeg konvatere alt om til dage
+          så jeg skal have lavet år og
+          måneder om til dage
+        */
+        /* 
+          Jeg har et array med mængden
+          af dage alle måneder (ud over dec).
+          jeg laver en let month som jeg
+          ligger alle dagene fra de
+          måneder vi har oversået i
+
+          conditionen er til for at skifte
+          mellem to arrays da jeg er nød
+          til at tage højde for skud år
+          så vis det nuværende år er en
+          del af 4-tabelen skal slå over
+          til det array hvor feb har 29 
+          dage
+        */
         let month = 0;
         if(year % 4 == 0){
           for(let i = 0; i < time[1] - 1; i++){
@@ -82,8 +109,31 @@ import { createCalendar } from '@/main.js' // maybe this make error
           }
         }
 
+        /* 
+          Jeg skal også tage højde for
+          år når det kommer til skudår
+          for ligesom febuar har et
+          skud år også en extra dag
+          så det er det 'leapStuff'
+          skal stå for
+
+          ellers vil den 31. dec på
+          et skud år være den samme
+          dag som den 1. jan året efter
+        */
         let leapStuff = Math.ceil((year - 2020) / 4)
 
+        /* 
+          Jeg centrere året omkring 2020
+          fordi det gør det nemmere at
+          arbejde med og vis der er behov
+          for debugging i fremtiden vil
+          det også være nemmere
+
+          Ellers konvertere jeg år om
+          til dage og ligger skudårs
+          dagene oven i
+        */
         let x = ((year - 2020) * 365) + leapStuff;
 
         const amountOfDays = x + (month) + (time[2] - 0)
@@ -117,10 +167,8 @@ import { createCalendar } from '@/main.js' // maybe this make error
       const onSubmit = async () => {
         if(form.end == ''){
           form.end = form.start
-          /* V få det ind i formen */
-          //amountOfDays = 1
         }
-        dayCalc()
+        console.log(dayCalc().amountOfDays)
         await createCalendar({ ...form }) 
         form.title = ''
         form.desc = ''
@@ -128,7 +176,7 @@ import { createCalendar } from '@/main.js' // maybe this make error
         form.end = ''
       }
 
-      return { form, onSubmit }
+      return { form, onSubmit, spawnDayAttachments }
     } 
   }
 </script>
