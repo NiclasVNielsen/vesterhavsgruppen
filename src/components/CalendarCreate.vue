a860dcea3eb8ae1c8410b7d469d68ef87dc1da04
NiclasNielsen <42369991+NiclasNielsen@users.noreply.github.com>
2021-05-13 19:58:14 +0200

    Calendar amount of days calculator

--- src/components/CalendarCreate.vue
+++ src/components/CalendarCreate.vue
@@ -53,27 +53,83 @@ import { reactive } from 'vue'
 import { createCalendar } from '@/main.js' // maybe this make error
 
   export default {
-   setup() {
-     const form = reactive({
-       title: '',
-       desc: '',
-       start: '',
-       end: ''
-     })
+    setup() {
+      const monthLengths = [31, 28, 31, 30, 31, 30, 31, 31, 30, 31, 30];
+      const monthLengthsLeap = [31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30];
 
-     const onSubmit = async () => {
-       if(form.end == ''){
-         form.end = form.start
-       }
-       await createCalendar({ ...form }) 
-       form.title = ''
-       form.desc = ''
-       form.start = ''
-       form.end = ''
-     }
+      const yearCalc = (time) => {
+        /* Leap years makes things complicated, okay?! */
+        /* 
+          Gotta be honest i just hit my head against
+          the wall and tried every theory that poped
+          up until i got something that worked.
 
-     return { form, onSubmit }
-   } 
+          It is tested it is working perfectly.
+          It is centered around 1. jan 2020
+          so anything before that is not tested
+          and will most likely not work
+        */
+
+        let year = time[0];
+        let month = 0;
+        if(year % 4 == 0){
+          for(let i = 0; i < time[1] - 1; i++){
+            month += monthLengthsLeap[i]
+          }
+        }else{
+          for(let i = 0; i < time[1] - 1; i++){
+            month += monthLengths[i]
+          }
+        }
+
+        let leapStuff = Math.ceil((year - 2020) / 4)
+
+        let x = ((year - 2020) * 365) + leapStuff;
+
+        const amountOfDays = x + (month) + (time[2] - 0)
+
+        return {
+          amountOfDays
+        }
+      }
+
+      const dayCalc = () => {
+
+        let start = form.start.split('-')
+        let end = form.end.split('-')
+
+        start = yearCalc(start).amountOfDays;
+        end = yearCalc(end).amountOfDays;
+
+        const amountOfDays = end - start + 1;
+        return {
+          amountOfDays
+        }
+      }
+
+      const form = reactive({
+        title: '',
+        desc: '',
+        start: '',
+        end: ''
+      })
+
+      const onSubmit = async () => {
+        if(form.end == ''){
+          form.end = form.start
+          /* V få det ind i formen */
+          //amountOfDays = 1
+        }
+        dayCalc()
+        await createCalendar({ ...form }) 
+        form.title = ''
+        form.desc = ''
+        form.start = ''
+        form.end = ''
+      }
+
+      return { form, onSubmit }
+    } 
   }
 </script>
 
