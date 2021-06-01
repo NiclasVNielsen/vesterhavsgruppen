0edd7a288b3b23ea794707f1df0c661e15c52150
NiclasNielsen <42369991+NiclasNielsen@users.noreply.github.com>
2021-06-01 09:35:33 +0200

    Ehhhm facebook embed.....
    Jesus hvorfor f*** skal det her være så dårligt?

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -78,7 +78,10 @@
                 Følg med på hvad der sker i gruppen
             </h4>
             <div>
-
+                <div class="post-container">
+                    <!-- Ehhmm jeg fandt den her linje kode her: https://stackoverflow.com/questions/19348278/automaticall-embed-newest-facebook-post-on-my-website -->
+                    <iframe src="http://www.facebook.com/plugins/likebox.php?href=http%3A%2F%2Fwww.facebook.com%2F908378059235877&width=500&colorscheme=light&show_faces=true&border_color&stream=true&header=true&height=560" scrolling="yes" style="border:none; overflow:hidden; width:500px; height:560px; background: white; float:left; " allowtransparency="true" frameborder="0"></iframe>
+                </div>
             </div>
         </section>
         <section class="signup">
@@ -142,6 +145,18 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+    .post-container{
+        position: relative;
+        min-height: calc(560px);
+        overflow: hidden;
+        iframe{
+            transform:  translate(-50%, -50%) scale(1);
+            position: absolute;
+            top: 50%;
+            left: 50%;
+        }
+    }
+
     .front-img{
         background: url('../assets/images/frontimage.jpg');
         background-size: cover;
