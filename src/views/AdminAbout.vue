6113548ac0ab7af62c9a019478a36a4a89c4f38a
NiclasNielsen <42369991+NiclasNielsen@users.noreply.github.com>
2021-06-03 10:08:49 +0200

    Om os

--- src/views/AdminAbout.vue
+++ src/views/AdminAbout.vue
@@ -1,88 +1,55 @@
 <template>
     <topping/>
     <div class="aboutpage">
-        <h3>
-            Om os
-        </h3>
         <form @submit.prevent="update">
+            <h3>
+                Om os
+            </h3>
             <div>
                 <label for="title">
                     Titel
                 </label>
-                <textarea v-model="form.title" type="text" required />
-                <label for="titletext">
-                    Titel tekst
-                </label>
+                <input v-model="form.title" type="text" required />
                 <textarea v-model="form.titletext" type="text" required />
             </div>
             <div>
                 <label for="kontakt">
                     Kontakt
                 </label>
-                <textarea v-model="form.kontakt" type="text" required />
+                <input v-model="form.kontakt" type="text" required />
             </div>
             <div>
                 <label for="boxone">
-                    Box 1 titel
+                    Box 1
                 </label>
-                <textarea v-model="form.boxone" type="text" required />
-                <label for="boxonenavn">
-                    Box 1 navn
-                </label>
-                <textarea v-model="form.boxonenavn" type="text" required />
-                <label for="boxonetlf">
-                    Box 1 tlf
-                </label>
-                <textarea v-model="form.boxonetlf" type="text" required />
-                <label for="boxoneemail">
-                    Box 1 email
-                </label>
-                <textarea v-model="form.boxoneemail" type="text" required />
+                <input v-model="form.boxone" type="text" required />
+                <input v-model="form.boxonenavn" type="text" required />
+                <input v-model="form.boxonetlf" type="text" required />
+                <input v-model="form.boxoneemail" type="text" required />
             </div>
             <div>
                 <label for="boxtwo">
-                    Box 2 titel
+                    Box 2
                 </label>
-                <textarea v-model="form.boxtwo" type="text" required />
-                <label for="boxtwonavn">
-                    Box 2 navn
-                </label>
-                <textarea v-model="form.boxtwonavn" type="text" required />
-                <label for="boxtwotlf">
-                    Box 2 tlf
-                </label>
-                <textarea v-model="form.boxtwotlf" type="text" required />
-                <label for="boxtwoemail">
-                    Box 2 email
-                </label>
-                <textarea v-model="form.boxtwoemail" type="text" required />
+                <input v-model="form.boxtwo" type="text" required />
+                <input v-model="form.boxtwonavn" type="text" required />
+                <input v-model="form.boxtwotlf" type="text" required />
+                <input v-model="form.boxtwoemail" type="text" required />
             </div>
             <div>
                 <label for="boxthree">
-                    Box 3 titel
+                    Box 3
                 </label>
-                <textarea v-model="form.boxthree" type="text" required />
-                <label for="boxthreenavn">
-                    Box 3 navn
-                </label>
-                <textarea v-model="form.boxthreenavn" type="text" required />
-                <label for="boxthreetlf">
-                    Box 3 tlf
-                </label>
-                <textarea v-model="form.boxthreetlf" type="text" required />
-                <label for="boxthreeemail">
-                    Box 3 email
-                </label>
-                <textarea v-model="form.boxthreeemail" type="text" required />
+                <input v-model="form.boxthree" type="text" required />
+                <input v-model="form.boxthreenavn" type="text" required />
+                <input v-model="form.boxthreetlf" type="text" required />
+                <input v-model="form.boxthreeemail" type="text" required />
             </div>
             <div>
                 <label for="sponsor">
                     Sponsor
                 </label>
-                <textarea v-model="form.sponsor" type="text" required />
-                <label for="sponsortext">
-                    Sponsor tekst
-                </label>
+                <input v-model="form.sponsor" type="text" required />
                 <textarea v-model="form.sponsortext" type="text" required />
             </div>
             <button type="submit">
@@ -154,3 +121,60 @@ export default {
     }
 }
 </script>
+
+<style lang="scss" scoped>
+    form{
+        width: 1000px;
+        margin: 50px auto;
+    }
+
+    h3{
+        margin-top: 50px;
+        margin-bottom: 20px;
+        font-size: 32px;
+        &.title{
+            font-size: 18px;
+        }
+        &:nth-of-type(2){
+            margin-top: 20px;
+        }
+    }
+
+    label{
+        display: block;
+        margin-top: 50px;
+    }
+
+    input, textarea{
+        display: block;
+        margin-bottom: 20px;
+    }
+
+    input{
+        width: 30em;
+    }
+
+    textarea{
+        resize: vertical;
+        width: 100%;
+        margin-bottom: 50px;
+        height: 9em;
+        &:last-of-type{
+            margin-bottom: 20px;
+        }
+    }
+    button{
+      display: flex;
+      justify-content: right;
+      width: 120px;
+      height: 30px;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      background: var(--prim);
+      color: var(--whiteshade1);
+      border-radius: 10px;
+      border: none;
+      font-size: .9em;
+    }
+</style>
